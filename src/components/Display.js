69ef54ad489613e7889075b2343ef185fbc30028
halfalpine <rocksandwoks@gmail.com>
2017-06-13 14:03:30 -0400

    Update Display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,11 +1,30 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Display extends Component {
-    render() {
-        return (
-            <div>I'm a Display</div>
-        );
+class Display extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {location} = this.props;
+    const {temp, rain, clouds} = this.props.forecast;
+    const conditions = if (this.props.forecast) {
+      <p>temp</p>
+      <p>rain>/p>
+      <p>clouds</p>
+    } else {
+      <p>no forecast yet!</p>
+      <p>placeholder</p>
     }
+
+    return (
+      <div>
+        <h1>Weather application</h1>
+        <p>This is the display.</p>
+        {conditions}
+      </div>
+    );
+  }
 }
 
 export default Display;

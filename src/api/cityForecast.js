4436fa92a16a7b00d8d31f08c4d099d54a4a8ae5
halfalpine <rocksandwoks@gmail.com>
2017-06-15 11:47:40 -0400

    Process json from API

--- src/api/cityForecast.js
+++ src/api/cityForecast.js
@@ -7,7 +7,7 @@ let getForecast = {
         let encodedLocation = encodeURIComponent(location);
         let url = `${OWA_URL}q=${encodedLocation}&appid=${API_KEY}`
         return fetchJsonp(url)
-            .then(conditions => conditions)
+            .then(conditions => conditions.json())
             // .catch(response => console.error(response));
     },
 
@@ -15,7 +15,7 @@ let getForecast = {
       console.log('in forCoordinates')
       let url = `${OWA_URL}lat=${xCoord}&lon=${yCoord}&appid=${API_KEY}`;
       return fetchJsonp(url)
-        .then(conditions => conditions)
+        .then(conditions => conditions.json())
     }
 }
 
